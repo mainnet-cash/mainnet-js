e14d15511d0822e014e8fe648aff5218c4318412
2qx <57997077+2qx@users.noreply.github.com>
2020-08-26 09:48:00 -0400

    re:7 new wallet named wallets api (#14)
    * untested libauth p2pk
    
    * update readme
    
    * wallet checkpoint
    
    * remove unused unlocking code
    
    * regtest with orphaning transaction
    
    * fix bchd data dir for other platforms
    
    * Working regtest
    
    * cleanup
    
    * Refactor wallet classes
    
    * prettier
    
    * isolate generated code
    
    * fix import and check express before tests

--- jest.node.setup.js
+++ jest.node.setup.js
@@ -1,6 +1,8 @@
 // jest.node.setup.js
 require("dotenv").config({ path: ".env.regtest" });
+
 const { spawn, spawnSync } = require("child_process");
+const http = require('http');
 
 function delay(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -18,6 +20,20 @@ async function pingBchd() {
     readinessArgs
   );
   return response.stderr;
+}
+
+async function serverReady() {
+  http.get('http://localhost:3000/openapi', (res) => {
+    let body = "";
+    res.on("data", data => {
+      body += data;
+    });
+    res.on("end", () => {
+     return res.statusCode === 200 ?  true : false
+    });
+  }).on("error", (err) => {
+    return false
+  });
 }
 
 module.exports = async function () {
@@ -40,15 +56,21 @@ module.exports = async function () {
     console.log("...already running");
   }
   if (global.mainnetServer === undefined) {
-    global.mainnetServer = spawn("npx", ["ts-node", "./serve/index.ts"], {
+    global.mainnetServer = spawn("npx", ["ts-node", "./generated/serve/index.ts"], {
       shell: false,
     });
+  }
+  // ping express 
+  for (let i = 0; (await serverReady()) || i > 10; i++) {
+    console.log("Waiting for express server");
+    await delay(1000);
   }
 
   // ping bchd as a readiness signal, give up and run anyway after 10s
   for (let i = 0; (await pingBchd()).length > 0 && i < 5; i++) {
-    console.log("Waiting for readiness");
+    console.log("Waiting for bchd node");
     await delay(2000);
   }
+
   console.log("proceeding...");
 };
