27703629c70fcb14d4da8acbe51c7ca38ef6caf5
2qx <2qx@small.neomailbox.ch>
2020-08-27 11:11:57 -0400

    merge in #7

--- serve/controllers/index.ts
+++
@@ -1,3 +0,0 @@
-export * as DefaultController from "./DefaultController";
-export * as SendController from "./SendController";
-export * as WalletController from "./WalletController";
--- serve/services/Service.ts
+++
@@ -1,9 +0,0 @@
-export class Service {
-  static rejectResponse(error: any, code = 500) {
-    return { error, code };
-  }
-
-  static successResponse(payload: any, code = 200) {
-    return { payload, code };
-  }
-}
