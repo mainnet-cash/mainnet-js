564f5a69d97322168349408d474cd1a5fccde209
2qx <2qx@small.neomailbox.ch>
2020-09-30 11:39:46 -0400

    chmod 755 on build

--- docker/start.sh
+++ docker/start.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-docker build docker/  --tag=fulcrum-regtest #--no-cache
-docker container rm regtest || true
-docker container create -p 60003:60003 --name regtest fulcrum-regtest 
 docker start --attach --interactive regtest
