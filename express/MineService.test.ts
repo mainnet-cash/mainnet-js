1e919db6bd8b983e34e028daac59b7e24f9a3895
2qx <2qx@users.noreply.github.com>
2020-10-06 15:33:02 +0000

    Apply formatting changes

--- express/MineService.test.ts
+++ express/MineService.test.ts
@@ -4,8 +4,8 @@ import { bchParam } from "../src/chain";
 import { SendRequest } from "../generated/client/typescript-mock/model/sendRequest";
 import { SendRequestItem } from "../generated/client/typescript-mock/model/sendRequestItem";
 import {
-    UtxoResponse,
-    UnitType,
+  UtxoResponse,
+  UnitType,
 } from "../generated/client/typescript-mock/api";
 
 var request = require("supertest");
@@ -13,39 +13,36 @@ var request = require("supertest");
 var app;
 
 describe("Post Endpoints", () => {
-    beforeAll(async function () {
-        app = await getServer().launch();
-    });
-    afterEach(function () {
-        app.close();
-    });
+  beforeAll(async function () {
+    app = await getServer().launch();
+  });
+  afterEach(function () {
+    app.close();
+  });
 
-    /**
-     * test mining blocks
-     */
-    it("Should return true from the readiness indicator", async () => {
-        const bobsWalletResp = await request(app).post("/v1/wallet/create").send({
-            name: "Bobs Regtest One Time Wallet",
-            type: mockApi.WalletRequest.TypeEnum.Wif,
-            network: mockApi.WalletRequest.NetworkEnum.Regtest,
-        });
-
-        const bobsCashaddr = bobsWalletResp.body.cashaddr;
-        const resp = await request(app).get("/mine").send({
-            cashaddr: bobsCashaddr,
-            blocks: 1
-        });
-
-
-        const bobBalanceResp = await request(app).post("/v1/wallet/balance").send({
-            walletId: bobsWalletResp.body.walletId,
-        });
-
-        expect(resp.statusCode).toEqual(200);
-        expect(resp.body.length).toEqual(1);
-        console.log(resp.body)
-        expect(bobBalanceResp.body.bch).toEqual(50);
+  /**
+   * test mining blocks
+   */
+  it("Should return true from the readiness indicator", async () => {
+    const bobsWalletResp = await request(app).post("/v1/wallet/create").send({
+      name: "Bobs Regtest One Time Wallet",
+      type: mockApi.WalletRequest.TypeEnum.Wif,
+      network: mockApi.WalletRequest.NetworkEnum.Regtest,
     });
 
+    const bobsCashaddr = bobsWalletResp.body.cashaddr;
+    const resp = await request(app).get("/mine").send({
+      cashaddr: bobsCashaddr,
+      blocks: 1,
+    });
 
+    const bobBalanceResp = await request(app).post("/v1/wallet/balance").send({
+      walletId: bobsWalletResp.body.walletId,
+    });
+
+    expect(resp.statusCode).toEqual(200);
+    expect(resp.body.length).toEqual(1);
+    console.log(resp.body);
+    expect(bobBalanceResp.body.bch).toEqual(50);
+  });
 });
