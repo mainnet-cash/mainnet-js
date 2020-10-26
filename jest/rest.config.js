496cf03de3e1a6becd5d1514c0daecc37e881413
2qx <2qx@users.noreply.github.com>
2020-10-26 08:20:19 +0000

    Apply formatting changes

--- jest/rest.config.js
+++ jest/rest.config.js
@@ -1,31 +1,30 @@
 module.exports = {
-    rootDir: "../",
-    collectCoverage: true,
-  
-    collectCoverageFrom: [
-      "**/*.{js,jsx,ts}",
-      "!**/node_modules/**",
-      "!**/generated/**",
-    ],
-    coveragePathIgnorePatterns: [
-      ".*/src/.*\\.d\\.ts",
-      ".*/src/.*\\.test\\.{ts,js}",
-      ".*/src/.*\\.test\\.headless\\.js",
-    ],
-    roots: ["<rootDir>/generated/serve/"],
-    testMatch: [
-      "**/__tests__/**/*.+(ts|tsx|js)",
-      "**/?(*.)+(spec|test).+(ts|tsx|js)",
-    ],
-    transform: {
-      "^.+\\.(ts|tsx)$": "ts-jest",
-    },
-    testEnvironment: "node",
-    setupFiles: ["fake-indexeddb/auto"],
-    globalSetup: "<rootDir>/jest/node.setup.js",
-    globalTeardown: "<rootDir>/jest/node.teardown.js",
-    verbose: true,
-    maxConcurrency: 1,
-    testTimeout: 65000,
-  };
-  
+  rootDir: "../",
+  collectCoverage: true,
+
+  collectCoverageFrom: [
+    "**/*.{js,jsx,ts}",
+    "!**/node_modules/**",
+    "!**/generated/**",
+  ],
+  coveragePathIgnorePatterns: [
+    ".*/src/.*\\.d\\.ts",
+    ".*/src/.*\\.test\\.{ts,js}",
+    ".*/src/.*\\.test\\.headless\\.js",
+  ],
+  roots: ["<rootDir>/generated/serve/"],
+  testMatch: [
+    "**/__tests__/**/*.+(ts|tsx|js)",
+    "**/?(*.)+(spec|test).+(ts|tsx|js)",
+  ],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  testEnvironment: "node",
+  setupFiles: ["fake-indexeddb/auto"],
+  globalSetup: "<rootDir>/jest/node.setup.js",
+  globalTeardown: "<rootDir>/jest/node.teardown.js",
+  verbose: true,
+  maxConcurrency: 1,
+  testTimeout: 65000,
+};
