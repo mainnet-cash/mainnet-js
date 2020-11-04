e29aa08fcc259fe2af8d55cfcd9d506e569ddf04
2qx <2qx@small.neomailbox.ch>
2020-11-04 12:35:40 -0500

    add more extensive tests

--- src/contract/escrow/EscrowContract.test.ts
+++ src/contract/escrow/EscrowContract.test.ts
@@ -3,13 +3,106 @@ import { RegTestWallet } from "../../wallet/Wif";
 import { Contract } from "cashscript";
 
 describe(`Test Escrow Contracts`, () => {
-  test("Should create a contract", async () => {
+  test("Should allow buyer to spend to seller", async () => {
     let funder = (await RegTestWallet.fromWIF(
       process.env.PRIVATE_WIF
     )) as RegTestWallet;
 
     let arbiter = (await RegTestWallet.newRandom()) as RegTestWallet;
     let buyer = (await RegTestWallet.newRandom()) as RegTestWallet;
+    let seller = (await RegTestWallet.newRandom()) as RegTestWallet;
+    let seller2 = (await RegTestWallet.newRandom()) as RegTestWallet;
+
+    await funder.send([
+      {
+        cashaddr: buyer.getDepositAddress()!,
+        value: 500000,
+        unit: "satoshis",
+      },
+    ]);
+    expect(await buyer.getBalance("sat")).toBe(500000);
+    let escrow = new EscrowContract({
+      arbiterCashaddr: arbiter.getDepositAddress()!,
+      buyerCashaddr: buyer.getDepositAddress()!,
+      sellerCashaddr: seller.getDepositAddress()!,
+    });
+
+    // fund the escrow contract
+    await buyer.send([
+      {
+        cashaddr: escrow.getAddress()!,
+        value: 450000,
+        unit: "satoshis",
+      },
+    ]);
+    expect(await escrow.getBalance()).toBe(450000);
+
+    // spend the escrow contract
+    await escrow.run(
+      buyer.privateKeyWif!,
+      "spendByBuyer"
+    );
+    expect(await escrow.getBalance()).toBe(0);
+    expect(await seller.getBalance("sat")).toBeGreaterThan(448000);
+
+    // spend the sellers funds to another wallet
+    await seller.sendMax(seller2.getDepositAddress()!)
+    expect(await seller2.getBalance("sat")).toBeGreaterThan(446000);
+
+  });
+
+  test("Should allow arbiter to spend to seller", async () => {
+    let funder = (await RegTestWallet.fromWIF(
+      process.env.PRIVATE_WIF
+    )) as RegTestWallet;
+
+    let arbiter = (await RegTestWallet.newRandom()) as RegTestWallet;
+    let buyer = (await RegTestWallet.newRandom()) as RegTestWallet;
+    let seller = (await RegTestWallet.newRandom()) as RegTestWallet;
+    let seller2 = (await RegTestWallet.newRandom()) as RegTestWallet;
+
+    await funder.send([
+      {
+        cashaddr: buyer.getDepositAddress()!,
+        value: 500000,
+        unit: "satoshis",
+      },
+    ]);
+    expect(await buyer.getBalance("sat")).toBe(500000);
+    let escrow = new EscrowContract({
+      arbiterCashaddr: arbiter.getDepositAddress()!,
+      buyerCashaddr: buyer.getDepositAddress()!,
+      sellerCashaddr: seller.getDepositAddress()!,
+    });
+
+    // fund the escrow contract
+    await buyer.send([
+      {
+        cashaddr: escrow.getAddress()!,
+        value: 450000,
+        unit: "satoshis",
+      },
+    ]);
+    expect(await escrow.getBalance()).toBe(450000);
+
+    // spend the escrow contract
+    await escrow.run(arbiter.privateKeyWif!, "spendByArbiter");
+    expect(await escrow.getBalance()).toBe(0);
+    expect(await seller.getBalance("sat")).toBeGreaterThan(448000);
+
+    // spend the sellers funds to another wallet
+    await seller.sendMax(seller2.getDepositAddress()!)
+    expect(await seller2.getBalance("sat")).toBeGreaterThan(446000);
+  });
+
+  test("Should allow seller to refund to buyer", async () => {
+    let funder = (await RegTestWallet.fromWIF(
+      process.env.PRIVATE_WIF
+    )) as RegTestWallet;
+
+    let arbiter = (await RegTestWallet.newRandom()) as RegTestWallet;
+    let buyer = (await RegTestWallet.newRandom()) as RegTestWallet;
+    let buyer2 = (await RegTestWallet.newRandom()) as RegTestWallet;
     let seller = (await RegTestWallet.newRandom()) as RegTestWallet;
 
     await funder.send([
@@ -19,13 +112,6 @@ describe(`Test Escrow Contracts`, () => {
         unit: "satoshis",
       },
     ]);
-    // await funder.send([{
-    //     cashaddr: arbiter.getDepositAddress()!,
-    //     value: 5000,
-    //     unit: "satoshis",
-    // }])
-
-    // expect(await arbiter.getBalance('sat')).toBe(5000);
     expect(await buyer.getBalance("sat")).toBe(500000);
     let escrow = new EscrowContract({
       arbiterCashaddr: arbiter.getDepositAddress()!,
@@ -33,6 +119,7 @@ describe(`Test Escrow Contracts`, () => {
       sellerCashaddr: seller.getDepositAddress()!,
     });
 
+    // fund the escrow contract
     await buyer.send([
       {
         cashaddr: escrow.getAddress()!,
@@ -40,24 +127,186 @@ describe(`Test Escrow Contracts`, () => {
         unit: "satoshis",
       },
     ]);
-
-    let contractUtxos = await escrow.getUtxos();
-
-    expect(contractUtxos.length).toBeGreaterThan(0);
     expect(await escrow.getBalance()).toBe(450000);
 
-    let buyerSpendResponse = await escrow.run(
-      buyer.privateKeyWif!,
-      "spendByBuyer",
-      false,
-      contractUtxos
-    );
-    //let arbiterSpendResponse = await escrow.run(arbiter.privateKeyWif!, "spendByArbiter", false, contractUtxos)
-
-    console.log(JSON.stringify(buyerSpendResponse));
-    //console.log(JSON.stringify(arbiterSpendResponse))
-    console.log(await seller.getBalance("sat"));
+    // refund the escrow contract
+    await escrow.run(seller.privateKeyWif!, "refundBySeller");
     expect(await escrow.getBalance()).toBe(0);
-    expect(await seller.getBalance("sat")).toBeGreaterThan(20);
+    expect(await buyer.getBalance("sat")).toBeGreaterThan(448000);
+
+    // spend the sellers funds to another wallet
+    await buyer.sendMax(buyer2.getDepositAddress()!)
+    expect(await buyer2.getBalance("sat")).toBeGreaterThan(446000);
+  });
+
+  test("Should allow arbiter to refund to buyer", async () => {
+    let funder = (await RegTestWallet.fromWIF(
+      process.env.PRIVATE_WIF
+    )) as RegTestWallet;
+
+    let arbiter = (await RegTestWallet.newRandom()) as RegTestWallet;
+    let buyer = (await RegTestWallet.newRandom()) as RegTestWallet;
+    let buyer2 = (await RegTestWallet.newRandom()) as RegTestWallet;
+    let seller = (await RegTestWallet.newRandom()) as RegTestWallet;
+
+    await funder.send([
+      {
+        cashaddr: buyer.getDepositAddress()!,
+        value: 500000,
+        unit: "satoshis",
+      },
+    ]);
+    expect(await buyer.getBalance("sat")).toBe(500000);
+    let escrow = new EscrowContract({
+      arbiterCashaddr: arbiter.getDepositAddress()!,
+      buyerCashaddr: buyer.getDepositAddress()!,
+      sellerCashaddr: seller.getDepositAddress()!,
+    });
+
+    // fund the escrow contract
+    await buyer.send([
+      {
+        cashaddr: escrow.getAddress()!,
+        value: 450000,
+        unit: "satoshis",
+      },
+    ]);
+    expect(await escrow.getBalance()).toBe(450000);
+
+    // refund the escrow contract
+    await escrow.run(arbiter.privateKeyWif!, "refundByArbiter");
+    expect(await escrow.getBalance()).toBe(0);
+    expect(await buyer.getBalance("sat")).toBeGreaterThan(448000);
+
+    // spend the sellers funds to another wallet
+    await buyer.sendMax(buyer2.getDepositAddress()!)
+    expect(await buyer2.getBalance("sat")).toBeGreaterThan(446000);
+  });
+
+  test("Should fail on refund by buyer", async () => {
+    expect.assertions(1);
+    try {
+      let funder = (await RegTestWallet.fromWIF(
+        process.env.PRIVATE_WIF
+      )) as RegTestWallet;
+
+      let arbiter = (await RegTestWallet.newRandom()) as RegTestWallet;
+      let buyer = (await RegTestWallet.newRandom()) as RegTestWallet;
+      let seller = (await RegTestWallet.newRandom()) as RegTestWallet;
+
+      await funder.send([
+        {
+          cashaddr: buyer.getDepositAddress()!,
+          value: 500000,
+          unit: "satoshis",
+        },
+      ]);
+      let escrow = new EscrowContract({
+        arbiterCashaddr: arbiter.getDepositAddress()!,
+        buyerCashaddr: buyer.getDepositAddress()!,
+        sellerCashaddr: seller.getDepositAddress()!,
+      });
+
+      // fund the escrow contract
+      await buyer.send([
+        {
+          cashaddr: escrow.getAddress()!,
+          value: 450000,
+          unit: "satoshis",
+        },
+      ]);
+
+      // refund the escrow contract
+      await escrow.run(buyer.privateKeyWif!, "refundByArbiter");
+    } catch (e) {
+      expect(e.message.split('\n')[0]).toBe(
+        "Error: Transaction failed with reason: the transaction was rejected by network rules."
+      );
+    }
+  });
+
+  test("Should throw error on spend by seller", async () => {
+    expect.assertions(1);
+    try {
+      let funder = (await RegTestWallet.fromWIF(
+        process.env.PRIVATE_WIF
+      )) as RegTestWallet;
+
+      let arbiter = (await RegTestWallet.newRandom()) as RegTestWallet;
+      let buyer = (await RegTestWallet.newRandom()) as RegTestWallet;
+      let seller = (await RegTestWallet.newRandom()) as RegTestWallet;
+
+      await funder.send([
+        {
+          cashaddr: buyer.getDepositAddress()!,
+          value: 500000,
+          unit: "satoshis",
+        },
+      ]);
+      let escrow = new EscrowContract({
+        arbiterCashaddr: arbiter.getDepositAddress()!,
+        buyerCashaddr: buyer.getDepositAddress()!,
+        sellerCashaddr: seller.getDepositAddress()!,
+      });
+
+      // fund the escrow contract
+      await buyer.send([
+        {
+          cashaddr: escrow.getAddress()!,
+          value: 450000,
+          unit: "satoshis",
+        },
+      ]);
+
+      // refund the escrow contract
+      await escrow.run(seller.privateKeyWif!, "spendByArbiter");
+    } catch (e) {
+      expect(e.message.split('\n')[0]).toBe(
+        "Error: Transaction failed with reason: the transaction was rejected by network rules."
+      );
+    }
+  });
+
+
+  test("Should throw error on spend by seller", async () => {
+    expect.assertions(1);
+    try {
+      let funder = (await RegTestWallet.fromWIF(
+        process.env.PRIVATE_WIF
+      )) as RegTestWallet;
+
+      let arbiter = (await RegTestWallet.newRandom()) as RegTestWallet;
+      let buyer = (await RegTestWallet.newRandom()) as RegTestWallet;
+      let seller = (await RegTestWallet.newRandom()) as RegTestWallet;
+
+      await funder.send([
+        {
+          cashaddr: buyer.getDepositAddress()!,
+          value: 500000,
+          unit: "satoshis",
+        },
+      ]);
+      let escrow = new EscrowContract({
+        arbiterCashaddr: arbiter.getDepositAddress()!,
+        buyerCashaddr: buyer.getDepositAddress()!,
+        sellerCashaddr: seller.getDepositAddress()!,
+      });
+
+      // fund the escrow contract
+      await buyer.send([
+        {
+          cashaddr: escrow.getAddress()!,
+          value: 450000,
+          unit: "satoshis",
+        },
+      ]);
+
+      // refund the escrow contract
+      await escrow.run(seller.privateKeyWif!, "spendByArbiter");
+    } catch (e) {
+      expect(e.message.split('\n')[0]).toBe(
+        "Error: Transaction failed with reason: the transaction was rejected by network rules."
+      );
+    }
   });
 });
--- src/contract/escrow/EscrowContract.ts
+++ src/contract/escrow/EscrowContract.ts
@@ -1,136 +1,142 @@
 import {
-  Contract,
-  SignatureTemplate,
-  CashCompiler,
-  ElectrumNetworkProvider,
-  Argument,
+    Contract, SignatureTemplate, CashCompiler,
+    ElectrumNetworkProvider,
+    Argument,
 } from "cashscript";
 import {
-  ElectrumCluster,
-  ElectrumTransport,
-  ClusterOrder,
+    ElectrumCluster,
+    ElectrumTransport,
+    ClusterOrder,
 } from "electrum-cash";
-import {
-  binToHex,
-  decodeCashAddress,
-  instantiateSecp256k1,
-} from "@bitauth/libauth";
+import { instantiateSecp256k1 } from "@bitauth/libauth"
 import { derivePublicKeyHash } from "../../util/derivePublicKeyHash";
+import { Utxo } from "../../interface"
 
 export class EscrowContract {
-  private buyerPKH: Uint8Array;
-  private arbiterPKH: Uint8Array;
-  private sellerPKH: Uint8Array;
-  private buyerCashaddr: string;
-  private sellerCashaddr: string;
 
-  constructor({
-    sellerCashaddr,
-    buyerCashaddr,
-    arbiterCashaddr,
-  }: {
-    sellerCashaddr: string;
-    buyerCashaddr: string;
-    arbiterCashaddr: string;
-  }) {
-    this.buyerPKH = derivePublicKeyHash(buyerCashaddr);
-    this.arbiterPKH = derivePublicKeyHash(arbiterCashaddr);
-    this.sellerPKH = derivePublicKeyHash(sellerCashaddr);
-    this.buyerCashaddr = buyerCashaddr;
-    this.sellerCashaddr = sellerCashaddr;
-  }
+    private buyerPKH: Uint8Array;
+    private arbiterPKH: Uint8Array;
+    private sellerPKH: Uint8Array;
+    private buyerCashaddr: string;
+    private sellerCashaddr: string;
 
-  public getAddress() {
-    const instance = this.getContactInstance();
-    return instance.address;
-  }
+    constructor({ sellerCashaddr, buyerCashaddr, arbiterCashaddr }: { sellerCashaddr: string, buyerCashaddr: string, arbiterCashaddr: string }) {
 
-  public getUtxos() {
-    const instance = this.getContactInstance();
-    return instance.getUtxos();
-  }
-  public getBalance() {
-    const instance = this.getContactInstance();
-    return instance.getBalance();
-  }
-
-  public async run(wif: string, funcName: string, getHexOnly = false, utxos) {
-    const instance = this.getContactInstance();
-    let fee = 2000;
-
-    const sig = new SignatureTemplate(wif);
-
-    let func;
-    const secp256k1 = await instantiateSecp256k1();
-    let publicKey = sig.getPublicKey(secp256k1);
-
-    if (typeof instance.functions[funcName] === "function") {
-      func = instance.functions[funcName];
-    } else {
-      throw Error(`${funcName} is not a contract method`);
-    }
-    let address;
-    if (funcName.startsWith("spend")) {
-      address = this.sellerCashaddr;
-    } else if (funcName.startsWith("refund")) {
-      address = this.buyerCashaddr;
+        this.buyerPKH = derivePublicKeyHash(buyerCashaddr)
+        this.arbiterPKH = derivePublicKeyHash(arbiterCashaddr)
+        this.sellerPKH = derivePublicKeyHash(sellerCashaddr)
+        this.buyerCashaddr = buyerCashaddr
+        this.sellerCashaddr = sellerCashaddr
     }
 
-    const method = getHexOnly ? "getTxHex" : "send";
-    const spendable = (await instance.getBalance()) - fee;
+    public getAddress() {
+        const instance = this.getContactInstance();
+        return instance.address;
+    }
 
-    if (utxos.length > 0) {
-      let funcInstance = func(fee, publicKey, sig)
-        .to(address, spendable)
-        .from(utxos);
-      try {
-        const tx: any = await funcInstance.withHardcodedFee(fee)[method]();
-      } catch (e) {
-        let fee;
-        const matcher = /Insufficient balance: available \((\d+)\) < needed \((\d+)\)./;
-        if (e.message.match(matcher)) {
-          const [, available, needed] = e.message.match(matcher);
-          fee = needed - available;
-          funcInstance = func(fee, publicKey, sig);
-          const tx: any = await funcInstance.withHardcodedFee(fee)[method]();
-          if (!getHexOnly) {
-            return { tx: tx.txid, fee: fee, utxo: utxos };
-          } else {
-            return tx;
-          }
-        } else {
-          throw Error(e);
+    public getUtxos() {
+        const instance = this.getContactInstance();
+        return instance.getUtxos();
+    }
+    public getBalance() {
+        const instance = this.getContactInstance();
+        return instance.getBalance();
+    }
+
+    public async run(wif: string, funcName: string, getHexOnly = false, utxos?: Utxo[]) {
+
+        const instance = this.getContactInstance();
+        let fee = 1200;
+
+        const sig = new SignatureTemplate(wif);
+        
+        
+        const secp256k1 = await instantiateSecp256k1();
+        let publicKey = sig.getPublicKey(secp256k1);
+        
+        
+        let func;
+        if (typeof instance.functions[funcName] === "function") {
+            func = instance.functions[funcName]
+        }else{
+            throw Error(`${funcName} is not a contract method`)
         }
-      }
-    } else {
-      return { utxo: null };
+
+        let address
+        if(funcName.startsWith("spend")){
+            address = this.sellerCashaddr
+        }else if(funcName.startsWith("refund")){
+            address = this.buyerCashaddr
+        }
+        
+        const method = getHexOnly ? 'getTxHex' : 'send';
+
+
+        const balance = await instance.getBalance()
+
+        if(typeof utxos === "undefined"){
+            utxos = await instance.getUtxos()
+        }
+
+        if (utxos.length > 0) {
+            try {
+                let funcInstance = func(fee, publicKey, sig).to(address, balance-fee).from(utxos);
+                const tx = await funcInstance.withHardcodedFee(fee)[method]()
+                if (getHexOnly) {
+                    return { tx: tx.txid, fee: fee, utxo: utxos };
+                } else {
+                    return tx;
+                }
+            } catch (e) {
+                const matcher = /Insufficient balance: available \((\d+)\) < needed \((\d+)\)./;
+                if (e.message.match(matcher)) {
+                    const [, available, needed] = e.message.match(matcher);
+                    fee = needed - available;
+                    let funcInstance = func(fee, publicKey, sig).to(address, balance-fee).from(utxos);
+                    let tx = await funcInstance.withHardcodedFee(fee)[method]()
+                    if (getHexOnly) {
+                        return { tx: tx.txid, fee: fee, utxo: utxos };
+                    } else {
+                        return tx;
+                    }
+    
+                } else {
+                    throw Error(e)
+                }
+            }            
+        }
+        else{
+            throw Error("There were no UTXOs provided or available on the contract")
+        }
+        
     }
-  }
 
-  private getContactInstance() {
-    const contractText = this.getContractText();
-    const artifact = CashCompiler.compileString(contractText);
-    const parameters: Argument[] = [
-      this.arbiterPKH,
-      this.buyerPKH,
-      this.sellerPKH,
-    ];
-    let cluster = new ElectrumCluster(
-      "CashScript Application",
-      "1.4.1",
-      1,
-      1,
-      ClusterOrder.RANDOM,
-      1020
-    );
-    cluster.addServer("127.0.0.1", 60003, ElectrumTransport.WS.Scheme, false);
-    const provider = new ElectrumNetworkProvider("testnet", cluster);
 
-    return new Contract(artifact, parameters, provider);
-  }
+    private getContactInstance() {
+        const contractText = this.getContractText();
+        const artifact = CashCompiler.compileString(contractText);
+        const parameters: Argument[] = [this.arbiterPKH, this.buyerPKH, this.sellerPKH]
+        let cluster = new ElectrumCluster(
+            "CashScript Application",
+            "1.4.1",
+            1,
+            1,
+            ClusterOrder.RANDOM,
+            1020
+        );
+        cluster.addServer(
+            "127.0.0.1",
+            60003,
+            ElectrumTransport.WS.Scheme,
+            false
+        );
+        const provider = new ElectrumNetworkProvider('testnet', cluster);
 
-  private getContractText() {
-    return `
+        return new Contract(artifact, parameters, provider);
+    }
+
+    private getContractText() {
+        return `
             pragma cashscript ^0.5.3;
             contract FundingContract(bytes20 arbiterPkh, bytes20 buyerPkh, bytes20 sellerPkh) {
 
@@ -172,5 +178,5 @@ export class EscrowContract {
                                 
             }
         `;
-  }
+    }
 }
