b0c0adc3f0775153be683459e11ea0b04644af55
mainnet_pat <mainnet_pat@protonmail.com>
2020-11-09 18:33:36 +0100

    Copy mainnet libraries into generated js api server upon build

--- generated/serve/services/WalletService.js
+++ generated/serve/services/WalletService.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 const Service = require('./Service');
-const mainnet = require("../../../dist/mainnet-node-0.0.1-rc");
+const mainnet = require("../lib/mainnet/mainnet-node-0.0.1-rc");
 
 /**
 * Get total balance for wallet
