c52ffb205c849deff25b8fc3d752130758b284a4
2qx <2qx@small.neomailbox.ch>
2020-12-23 16:56:39 -0500

    Move dust to fee if below threshold

--- src/constant.ts
+++ src/constant.ts
@@ -1,3 +1,5 @@
 export const GROUP_DELIMITER = "␝";
 export const ROW_DELIMITER = "␞";
 export const UNIT_DELIMITER = "␟";
+
+export const DUST_UTXO_THRESHOLD = 546;
--- src/transaction/Wif.ts
+++ src/transaction/Wif.ts
@@ -14,6 +14,7 @@ import {
 } from "@bitauth/libauth";
 import { UtxoI } from "../interface";
 
+import { DUST_UTXO_THRESHOLD } from "../constant"
 import { SendRequest } from "../wallet/model";
 import { amountInSatoshi } from "../util/amountInSatoshi";
 import { sumSendRequestAmounts } from "../util/sumSendRequestAmounts";
@@ -56,10 +57,12 @@ export async function buildP2pkhNonHdTransaction(
     if (discardChange !== true) {
       const changeAmount =
         BigInt(inputAmount) - BigInt(sendAmount) - BigInt(fee);
-      lockedOutputs.push({
-        lockingBytecode: changeLockingBytecode.bytecode,
-        satoshis: bigIntToBinUint64LE(BigInt(changeAmount)),
-      });
+      if(changeAmount > DUST_UTXO_THRESHOLD){
+        lockedOutputs.push({
+          lockingBytecode: changeLockingBytecode.bytecode,
+          satoshis: bigIntToBinUint64LE(BigInt(changeAmount)),
+        });  
+      }
     }
 
     let signedInputs = prepareInputs(inputs, compiler, signingKey);
@@ -164,6 +167,7 @@ export async function getSuitableUtxos(
       break;
     }
   }
+
   // If the amount needed is met, or no amount is given, return
   if (typeof amountRequired === "undefined") {
     return suitableUtxos;
@@ -192,7 +196,7 @@ export async function getFeeAmount({
       utxos,
       sendRequests,
       privateKey,
-      1000
+      1000 
     );
     return draftTransaction.length * 1 + 1;
   } else {
