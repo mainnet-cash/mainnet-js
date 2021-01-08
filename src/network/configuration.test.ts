9ed83297846dd2f38e4fcf49b1bd4ccdc9732b8a
2qx <2qx@small.neomailbox.ch>
2021-01-08 12:10:10 -0500

    get electum servers from ENV if available

---
+++ src/network/configuration.test.ts
@@ -0,0 +1,35 @@
+import * as config from "./configuration";
+import * as primary from "./constant";
+
+test("Should get electrum settings from defaults", async () => {
+  let s = config.getDefaultServers();
+  expect(s.mainnet).toBe(primary.mainnetServers);
+  expect(s.testnet).toBe(primary.testnetServers);
+  expect(s.regtest).toBe(primary.regtestServers);
+});
+
+test("Should get electrum settings from env", async () => {
+  process.env.ELECTRUM = "https://example.com:1234";
+  process.env.ELECTRUM_TESTNET =
+    "https://test.example.com:1234,https://test.example.dk:1234";
+  process.env.ELECTRUM_REGTEST = "ws://reg.example.com:1234";
+  let s = config.getDefaultServers();
+  expect(s.mainnet).toStrictEqual(["https://example.com:1234"]);
+  expect(s.testnet).toStrictEqual([
+    "https://test.example.com:1234",
+    "https://test.example.dk:1234",
+  ]);
+  expect(s.regtest).toStrictEqual(["ws://reg.example.com:1234"]);
+});
+
+test("Should get electrum cluster confidence from defaults", async () => {
+    let c = config.getConfidence()
+    expect(c).toStrictEqual(1);
+  });
+  
+  test("Should get electrum cluster confidence from env", async () => {
+    process.env.ELECTRUM_CONFIDENCE = "2";
+    let c = config.getConfidence()
+    expect(c).toStrictEqual(2);
+  });
+  
---
+++ src/network/configuration.ts
@@ -0,0 +1,48 @@
+import { getPlatform } from "../util";
+import * as primary from "./constant";
+
+let mainnetServers: string[],
+  testnetServers: string[],
+  regtestServers: string[];
+
+export function getDefaultServers() {
+  if (getPlatform() == "node") {
+    mainnetServers = process.env.ELECTRUM
+      ? process.env.ELECTRUM.split(",")
+      : primary.mainnetServers;
+    testnetServers = process.env.ELECTRUM_TESTNET
+      ? process.env.ELECTRUM_TESTNET.split(",")
+      : primary.testnetServers;
+    regtestServers = process.env.ELECTRUM_REGTEST
+      ? process.env.ELECTRUM_REGTEST.split(",")
+      : primary.regtestServers;
+  } else {
+    mainnetServers = primary.mainnetServers;
+    testnetServers = primary.testnetServers;
+    regtestServers = primary.regtestServers;
+  }
+  return {
+    mainnet: mainnetServers,
+    testnet: testnetServers,
+    regtest: regtestServers,
+  };
+}
+
+export const ELECTRUM_USER_AGENT = process.env.ELECTRUM_USER_AGENT
+  ? process.env.ELECTRUM_USER_AGENT
+  : "mainnet-js-" + getPlatform();
+
+export function getConfidence() {
+  // Allow users to configure the cluster confidence
+  let confidence;
+  if (getPlatform() === "node") {
+    confidence = process.env.ELECTRUM_CONFIDENCE
+      ? parseInt(process.env.ELECTRUM_CONFIDENCE)
+      : 1;
+  } else {
+    confidence = 1;
+  }
+  return confidence;
+}
+
+export const defaultServers = getDefaultServers();
