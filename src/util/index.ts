ac101a4cdb177c74f735696fec0b8ce79a2417e9
2qx <2qx@small.neomailbox.ch>
2021-01-09 02:54:50 -0500

    remove getPlatform

--- src/util/index.ts
+++ src/util/index.ts
@@ -1,5 +1,4 @@
 export { convert, convertObject } from "./convert";
 export { getRuntimePlatform } from "./getRuntimePlatform";
 export { getUsdRate } from "./getUsdRate";
-export { getPlatform } from "./getPlatform";
 export { serializeUtxo, deserializeUtxo } from "./serializeUtxo";
