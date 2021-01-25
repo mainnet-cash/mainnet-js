74552d7f686d54142a723d014c263ff3914b2e81
mainnet-pat <mainnet-pat@users.noreply.github.com>
2021-01-25 21:35:11 +0000

    Apply formatting changes

--- polyfill/support/types.js
+++ polyfill/support/types.js
@@ -3,16 +3,20 @@
 
 "use strict";
 
-const nativeBigInt = (typeof global !== 'undefined') && global.BigInt || (typeof window !== 'undefined') && window.BigInt;
-const supportsNativeBigInt = typeof nativeBigInt === 'function';
+const nativeBigInt =
+  (typeof global !== "undefined" && global.BigInt) ||
+  (typeof window !== "undefined" && window.BigInt);
+const supportsNativeBigInt = typeof nativeBigInt === "function";
 
 if (!supportsNativeBigInt) {
   var _BigInt = require("bigint-polyfill");
-  globalThis.BigInt = function(...args) { return new _BigInt(...args) };
+  globalThis.BigInt = function (...args) {
+    return new _BigInt(...args);
+  };
 }
 
-if ((typeof global !== 'undefined') || (typeof window !== 'undefined')) {
-  globalThis.Buffer = require('buffer/').Buffer;
+if (typeof global !== "undefined" || typeof window !== "undefined") {
+  globalThis.Buffer = require("buffer/").Buffer;
 }
 
 var isArgumentsObject = require("is-arguments");
