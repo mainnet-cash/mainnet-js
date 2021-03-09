4d8195a52f5c9688e23efb1a39ce256e90fe3618
2qx <2qx@small.neomailbox.ch>
2021-03-09 11:28:45 -0500

    doc strings for signatures

--- src/message/signed.ts
+++ src/message/signed.ts
@@ -12,6 +12,17 @@ import {
 import { derivePrefix } from "../util/derivePublicKeyHash";
 import { hash160 } from "../util/hash160";
 
+/**
+ * message_magic - "Magic" per standard bitcoin message signing.
+ * 
+ * In this case, the magic is simply adding "b'24' + Bitcoin Signed Message\n" followed
+ * by the size of the message in binary and the message encoded as binary.
+ *
+ * @param {str} string    The string to add the magic syntax to.
+ *
+ * @returns a promise to the string as binary array with magic syntax
+ */
+// see    https://github.com/Electron-Cash/Electron-Cash/blob/49f9f672364f50053a026e4a5cb30e92db2d195d/electroncash/bitcoin.py#L524
 function message_magic(str: string) {
   const length = utf8ToBin(str).length.toString(16);
   let payload = `\x18Bitcoin Signed Message:\n`;
@@ -22,12 +33,29 @@ function message_magic(str: string) {
   ]);
 }
 
+/**
+ * hash_magic - Return the hash of the string that has undergone standard formatting
+ *
+ * @param {str} string    The string to hash
+ *
+ * @returns a promise to the hash of the string.
+ */
 export async function hash_magic(str: string) {
   const h = (await instantiateSha256()).hash;
   return h(h(message_magic(str)));
 }
 
 export class SignedMessage {
+
+
+/**
+ * sign - Calculate the recoverable signed checksum of a string message.
+ *
+ * @param {message} string          The 
+ * @param {privateKey} Uint8Array   The private key to sign the message with
+ *
+ * @returns a promise to signature as a string
+ */
   public static async sign(
     message: string,
     privateKey: Uint8Array
@@ -42,12 +70,22 @@ export class SignedMessage {
     return binToBase64(rs.signature);
   }
 
-  // see    https://github.com/Electron-Cash/Electron-Cash/blob/49f9f672364f50053a026e4a5cb30e92db2d195d/electroncash/bitcoin.py#L524
+/**
+ * verify - Validate that the message is valid
+ *
+ * @param {message} string     The message to verify as a utf8 string
+ * @param {signature} string   The signature as a base64 encoded string
+ * @param {cashaddr} string    The cashaddr to validate the signature against.
+ *
+ * @returns a promise to signature as a string
+ */
   public static async verify(
     message: string,
     signature: string,
     cashaddr: string
   ): Promise<boolean> {
+
+    // Check that the signature is valid for the given message.
     const secp256k1 = await instantiateSecp256k1();
     let messageHash = await hash_magic(message);
     let sig = base64ToBin(signature);
@@ -55,16 +93,15 @@ export class SignedMessage {
     let recoveryId = prefix === "bitcoincash" ? 0 : (1 as RecoveryId);
     let pk = secp256k1.recoverPublicKeyCompressed(sig, recoveryId, messageHash);
     let pkh = await hash160(pk);
-    let resultingCashaddr = encodeCashAddress(prefix, 0, pkh);
+    let valid = secp256k1.verifySignatureCompact(sig, pk, messageHash);
 
+    // Validate that the signature actually matches the provided cashaddr
+    let resultingCashaddr = encodeCashAddress(prefix, 0, pkh);
     if (resultingCashaddr !== cashaddr) {
       console.log("cashaddr match failed");
       return false;
     }
-
-    //return derivedPkh === pkh
-    let valid = secp256k1.verifySignatureCompact(sig, pk, messageHash);
-
+    
     return valid;
   }
 }
--- src/util/hash160.ts
+++ src/util/hash160.ts
@@ -1,5 +1,13 @@
 import { instantiateSha256, instantiateRipemd160 } from "@bitauth/libauth";
 
+
+/**
+ * hash160 - Calculate the sha256, ripemd160 hash of a value
+ *
+ * @param {message} Uint8Array       value to hash as a binary array
+ *
+ * @returns a promise to the hash160 value of the input
+ */
 export async function hash160(message: Uint8Array) {
   const ripemd160 = await instantiateRipemd160();
   const sha256 = await instantiateSha256();
