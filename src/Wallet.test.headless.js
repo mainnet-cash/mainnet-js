9e24995da472fdb0b71169da89f9ad0ee27469a2
2qx <2qx@small.neomailbox.ch>
2021-03-13 09:43:35 -0500

    fix browser test

--- src/Wallet.test.headless.js
+++ src/Wallet.test.headless.js
@@ -206,14 +206,14 @@ describe(`Wallet should function in the browser`, () => {
       const alice = await walletFromId(`wif:regtest:${wif}`);
       let result = await alice.sign("test");
       return {
-        valid: await alice.verify("test", result.signature),
+        resp: await alice.verify("test", result.signature),
         signature: result.signature,
       };
     }, process.env.PRIVATE_WIF);
     expect(result.signature).toBe(
       "IOEEiqRXRVK9gPUNpXuBjJUK47Y8XpseZejgwu59CoNSVv+3K1NkHdT64RXHP7cw4PZ6usRQ4ULrP/p5CJnrg9U="
     );
-    expect(result.valid).toBe(true);
+    expect(result.resp.valid).toBe(true);
   });
 
   test(`Should send to Bob; sendMax all of Bob's funds back`, async () => {
