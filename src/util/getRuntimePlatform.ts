424969da9e37456e69ab37bf83d33156fbb59077
2qx <2qx@small.neomailbox.ch>
2021-03-24 10:22:27 -0400

    add reactNative platform check

--- src/util/getRuntimePlatform.ts
+++ src/util/getRuntimePlatform.ts
@@ -2,6 +2,7 @@ export enum RuntimePlatform {
   node = "node",
   browser = "browser",
   webworker = "webworker",
+  app = "reactNative"
 }
 
 export function getRuntimePlatform(): RuntimePlatform {
@@ -20,9 +21,17 @@ export function getRuntimePlatform(): RuntimePlatform {
     // ) {
     //   return RuntimePlatform.webworker;
     // } else
-    if ("undefined" !== typeof navigator && "undefined" !== typeof document) {
-      return RuntimePlatform.browser;
-    } else {
+    if ("undefined" !== typeof navigator){
+      if (navigator.product == 'ReactNative'){
+        return RuntimePlatform.app
+      }
+      else if("undefined" !== typeof document) {
+        return RuntimePlatform.browser;
+      } else{
+        throw Error("Could not determine runtime platform");
+      }
+    } 
+    else {
       throw Error("Could not determine runtime platform");
     }
   }
