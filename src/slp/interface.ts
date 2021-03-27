75c3996bdd90843be25ec28af68e488dd081b76a
mainnet-pat <mainnet_pat@protonmail.com>
2021-03-27 05:47:52 +0000

    Fix interface import

--- src/slp/interface.ts
+++ src/slp/interface.ts
@@ -1,6 +1,5 @@
 import BigNumber from "bignumber.js";
-import { TxI } from "mainnet-js/interface";
-import { UtxoI } from "../interface";
+import { UtxoI, TxI } from "../interface";
 
 export enum SlpTokenType {
   Type1 = 0x01,
