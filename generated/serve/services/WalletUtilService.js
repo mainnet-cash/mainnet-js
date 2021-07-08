86b3a44b40ac4da3a6f373b0697d161da4e4fd11
mainnet-pat <mainnet_pat@protonmail.com>
2021-07-08 18:15:16 +0000

    Add decodeTransaction method, tests, rest api and api tests

---
+++ generated/serve/services/WalletUtilService.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-unused-vars */
+const Service = require('./Service');
+const mainnet = require("mainnet-js");
+
+/**
+* Decode a bitcoin transaction. Accepts both transaction hash or raw transaction in hex format.
+*
+* utilDecodeTransactionRequest UtilDecodeTransactionRequest Request to decode a transaction 
+* returns ElectrumRawTransaction
+* */
+const utilDecodeTransaction = ({ utilDecodeTransactionRequest }) => new Promise(
+  async (resolve, reject) => {
+    try {
+      const wallet = await mainnet.createWallet({ network: utilDecodeTransactionRequest.network });
+      const resp = await wallet.util.decodeTransaction(utilDecodeTransactionRequest.transactionHashOrHex);
+      resolve(Service.successResponse({ ...resp }));
+    } catch (e) {
+      reject(Service.rejectResponse(
+        e.message || 'Invalid input',
+        e.status || 405,
+      ));
+    }
+  },
+);
+
+module.exports = {
+  utilDecodeTransaction,
+};
---
+++ src/wallet/Util.ts
@@ -0,0 +1,36 @@
+import {
+  Wallet,
+} from "../wallet/Wif";
+import { binToHex, hexToBin, instantiateSha256, instantiateSha256Bytes, Sha256 } from "@bitauth/libauth";
+import { ElectrumRawTransaction } from "../network/interface";
+
+/**
+ * Class with various wallet utilities.
+ */
+export class Util {
+  readonly wallet: Wallet;
+  /**
+   * Initializes a wallet Util class.
+   *
+   * @param wallet     A wallet object
+   */
+  constructor(wallet: Wallet) {
+    this.wallet = wallet;
+  }
+
+  public async getTransactionHash(rawTransactionHex: string): Promise<string> {
+    const transactionBin = hexToBin(rawTransactionHex);
+
+    const sha256 = await instantiateSha256();
+    // transaction hash is a double sha256 of a raw transaction data, reversed byte order
+    return binToHex(sha256.hash(sha256.hash(transactionBin)).reverse());
+  }
+
+  public async decodeTransaction(transactionHashOrHex: string): Promise<ElectrumRawTransaction> {
+    if (transactionHashOrHex.length > 64) {
+      transactionHashOrHex = await this.getTransactionHash(transactionHashOrHex);
+    }
+
+    return await this.wallet.provider!.getRawTransactionObject(transactionHashOrHex);
+  }
+}
