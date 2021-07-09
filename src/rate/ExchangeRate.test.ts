ecd48fe7fb4e1d51169e109b73d1f851adfd3d5d
mainnet-pat <mainnet_pat@protonmail.com>
2021-07-09 12:06:19 +0000

    Add axios mocks for currency convercion remote calls

--- src/rate/ExchangeRate.test.ts
+++ src/rate/ExchangeRate.test.ts
@@ -1,6 +1,9 @@
 import { ExchangeRate } from "./ExchangeRate";
 
 test("Get price in usd", async () => {
+  ExchangeRate.setupAxiosMock("https://api.coingecko.com/api/v3/simple/price?ids=bitcoin-cash&vs_currencies=usd", { 'bitcoin-cash': { usd: 666.666 } });
+  ExchangeRate.setupAxiosMock("https://markets.api.bitcoin.com/live/bitcoin",  { BCH: 666.666 });
+
   let rate = await ExchangeRate.get("usd");
-  expect(rate).toBeGreaterThan(75.0);
+  expect(rate).toBe(666.666);
 });
