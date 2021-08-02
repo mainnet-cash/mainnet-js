e2887bc256599416a879a1cef2b32a780e32bac2
mainnet-pat <74184164+mainnet-pat@users.noreply.github.com>
2021-08-02 16:27:38 +0300

    Support library compression in production builds (#123)
    * Compress built library
    
    * Move compression to the base config
    
    * Bump version, update release notes
    
    Co-authored-by: mainnet-pat <mainnet-pat@users.noreply.github.com>

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const { merge } = require("webpack-merge");
 const packageJson = require("./package.json");
 const NpmDtsPlugin = require("npm-dts-webpack-plugin");
+const CompressionWebpackPlugin = require("compression-webpack-plugin");
 
 const baseConfig = {
   entry: "./src/index.ts",
@@ -22,6 +23,12 @@ const baseConfig = {
     mangleWasmImports: true,
     usedExports: true,
   },
+  plugins: [
+    new CompressionWebpackPlugin({
+      algorithm: "gzip",
+      test: /\.(js)$/,
+    }),
+  ],
 };
 
 const prodConfig = {
