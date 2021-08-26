8f3f716f0772ec7b14e12bfb79701334f26bd170
mainnet-pat <mainnet_pat@protonmail.com>
2021-08-26 15:19:37 +0000

    Add expects available in browser context

---
+++ src/test/test.ts
@@ -0,0 +1,24 @@
+export function expect(expected) {
+  return {
+    toBe: function(received) {
+      if (!(expected === received)) {
+        throw new Error(`assertion failed: expected ${expected} is not equal to received ${received}`);
+      }
+    },
+    toBeGreaterThan: function(received) {
+      if (!(expected > received)) {
+        throw new Error(`assertion failed: expected ${expected} is not GreaterThan received ${received}`);
+      }
+    },
+    toBeGreaterThanOrEqual: function(received) {
+      if (!(expected >= received)) {
+        throw new Error(`assertion failed: expected ${expected} is not GreaterThanOrEqual received ${received}`);
+      }
+    },
+    toStrictEqual: function(received) {
+      if (!(JSON.stringify(expected) === JSON.stringify(received))) {
+        throw new Error(`assertion failed: expected ${expected} is not StrictEqual to received ${received}`);
+      }
+    }
+  }
+}
