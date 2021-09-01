7d1d4e52e89bda7c1c089584a75bf7b801958c40
mainnet-pat <mainnet_pat@protonmail.com>
2021-09-01 15:02:22 +0000

    Update and republish gspp and smartbch docker images

--- jest/docker/gspp/build.sh
+++ jest/docker/gspp/build.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+# docker buildx build -t mainnet/gspp --platform linux/arm64/v8 --load .
+# docker buildx build -t mainnet/gspp --platform linux/amd64 --load .
+
 docker buildx build -t mainnet/gspp --platform linux/arm64/v8,linux/amd64 --push .
--- jest/docker/smartbch/build.sh
+++ jest/docker/smartbch/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-docker buildx build --progress plain -t mainnet/smartbch --platform=linux/arm64/v8 --load .
-docker buildx build --progress plain -t mainnet/smartbch --platform=linux/amd64 --load .
+# docker buildx build --progress plain -t mainnet/smartbch --platform=linux/amd64 --load .
+# docker buildx build --progress plain -t mainnet/smartbch --platform=linux/arm64/v8 --load .
 
 docker buildx build --progress plain -t mainnet/smartbch --platform=linux/arm64/v8,linux/amd64 --push .
