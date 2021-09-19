729fdb3a55e66e2a8bec6653d6b903a0fd01369a
mainnet-pat <mainnet-pat@users.noreply.github.com>
2021-09-19 16:37:23 +0000

    Apply formatting changes

--- src/smartbch/Network.ts
+++ src/smartbch/Network.ts
@@ -3,7 +3,10 @@ import { NetworkType } from "../enum";
 
 export let defaultServers = {
   EthMainnet: ["https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161"],
-  mainnet: ["https://smartbch.fountainhead.cash/mainnet", "https://smartbch.greyh.at"],
+  mainnet: [
+    "https://smartbch.fountainhead.cash/mainnet",
+    "https://smartbch.greyh.at",
+  ],
   testnet: ["http://35.220.203.194:8545", "https://moeing.tech:9545"],
   regtest: ["http://127.0.0.1:8545"],
 };
@@ -14,34 +17,29 @@ export function getNetworkProvider(
   const url = defaultServers[network][0] || "";
   switch (network as any) {
     case "EthMainnet": {
-      return new ethers.providers.JsonRpcProvider(
-        url
-      );
+      return new ethers.providers.JsonRpcProvider(url);
     }
     case NetworkType.Mainnet: {
       // return new ethers.providers.WebSocketProvider(
       //   "wss://smartbch-wss.greyh.at",
       //   { name: "smartbch", chainId: 10000 }
       // );
-      return new ethers.providers.JsonRpcProvider(
-        url,
-        { name: "smartbch", chainId: 10000 }
-      );
+      return new ethers.providers.JsonRpcProvider(url, {
+        name: "smartbch",
+        chainId: 10000,
+      });
     }
     case NetworkType.Testnet: {
-      return new ethers.providers.JsonRpcProvider(
-        url,
-        { name: "smartbch", chainId: 10001 }
-      );
+      return new ethers.providers.JsonRpcProvider(url, {
+        name: "smartbch",
+        chainId: 10001,
+      });
     }
     default: {
-      return new ethers.providers.StaticJsonRpcProvider(
-        url,
-        {
-          name: "smartbch",
-          chainId: 10002,
-        }
-      );
+      return new ethers.providers.StaticJsonRpcProvider(url, {
+        name: "smartbch",
+        chainId: 10002,
+      });
     }
   }
 }
