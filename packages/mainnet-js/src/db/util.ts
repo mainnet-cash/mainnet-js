a39416d8369a0e35333bf2158b73731529c2782f
2qx <2qx@users.noreply.github.com>
2021-09-30 14:44:27 +0000

    Apply formatting changes

--- packages/mainnet-js/src/db/util.ts
+++ packages/mainnet-js/src/db/util.ts
@@ -23,16 +23,22 @@ export function indexedDbIsAvailable() {
   return "indexedDB" in globalThis;
 }
 
-export function getSslConfig() : sslConfigI {
-
-  const ca = process.env.DATABASE_SSL_CA ? Buffer.from(process.env.DATABASE_SSL_CA, "base64").toString("ascii") : undefined
-  const key = process.env.DATABASE_SSL_KEY ? Buffer.from(process.env.DATABASE_SSL_KEY, "base64").toString("ascii") : undefined
-  const cert = process.env.DATABASE_SSL_CERT ? Buffer.from(process.env.DATABASE_SSL_CERT, "base64").toString("ascii") : undefined
+export function getSslConfig(): sslConfigI {
+  const ca = process.env.DATABASE_SSL_CA
+    ? Buffer.from(process.env.DATABASE_SSL_CA, "base64").toString("ascii")
+    : undefined;
+  const key = process.env.DATABASE_SSL_KEY
+    ? Buffer.from(process.env.DATABASE_SSL_KEY, "base64").toString("ascii")
+    : undefined;
+  const cert = process.env.DATABASE_SSL_CERT
+    ? Buffer.from(process.env.DATABASE_SSL_CERT, "base64").toString("ascii")
+    : undefined;
   let ssl: sslConfigI = {
-    rejectUnauthorized : process.env.DATABASE_SSL_REJECT_UNAUTHORIZED == "false" ? false : true,
-    ca : ca,
-    key : key,
-    cert : cert
-  }
-  return ssl
+    rejectUnauthorized:
+      process.env.DATABASE_SSL_REJECT_UNAUTHORIZED == "false" ? false : true,
+    ca: ca,
+    key: key,
+    cert: cert,
+  };
+  return ssl;
 }
