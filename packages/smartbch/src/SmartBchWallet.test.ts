d5b0688ac2defa47774d2ef91ed8ac5b0853c18b
mainnet-pat <mainnet-pat@users.noreply.github.com>
2021-10-06 19:19:50 +0000

    Apply formatting changes

--- packages/smartbch/src/SmartBchWallet.test.ts
+++ packages/smartbch/src/SmartBchWallet.test.ts
@@ -4,7 +4,7 @@ import {
   RegTestSmartBchWallet,
   SmartBchWallet,
   TestNetSmartBchWallet,
-  BalanceResponse
+  BalanceResponse,
 } from "./";
 import { JsonRpcProvider } from "@ethersproject/providers";
 
