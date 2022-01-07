9f2d5c1863f06a30421498ca22f2b91390cb8fa6
2qx <57997077+2qx@users.noreply.github.com>
2022-01-07 10:17:17 -0500

    Feature/derive public node child (#143)
    * xpubkey info and derivation
    
    * Update generated
    
    * bump version
    
    * Apply formatting changes
    
    * add wallet/xpubkeys endpoint
    
    * Update generated
    
    * Apply formatting changes
    
    * modify error to clarify implications
    
    * fix error test
    
    Co-authored-by: 2qx <2qx@users.noreply.github.com>

--- packages/mainnet-js/src/wallet/Wif.test.ts
+++ packages/mainnet-js/src/wallet/Wif.test.ts
@@ -390,7 +390,7 @@ describe(`XPubKey path derivation`, () => {
       let commonPaths = await w.getXPubKeys(["m"]);
     } catch (e: any) {
       expect(e.message).toBe(
-        "Usage of the root extended public key may lead to loss of funds. Storing or sharing the root public key is strongly discouraged. See: https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki#implications"
+        "Storing or sharing of parent public key may lead to loss of funds. Storing or sharing *root* parent public keys is strongly discouraged, although all parent keys have risk. See: https://github.com/bitcoin/bips/blob/master/bip-0032.mediawiki#implications"
       );
     }
   });
