a588d530a3e28c622ec80c6befe0c39b049e7959
2qx <2qx@users.noreply.github.com>
2022-02-10 19:36:43 +0000

    Apply formatting changes

--- packages/mainnet-js/src/network/constant.ts
+++ packages/mainnet-js/src/network/constant.ts
@@ -8,10 +8,12 @@ export const networkTickerMap = {
   regtest: "rBCH",
 };
 
-export const mainnetServers = ["wss://fulcrum.fountainhead.cash",
-"wss://bch.imaginary.cash:50004",
-"wss://electrum.imaginary.cash:50004",
-"wss://blackie.c3-soft.com:50004"];
+export const mainnetServers = [
+  "wss://fulcrum.fountainhead.cash",
+  "wss://bch.imaginary.cash:50004",
+  "wss://electrum.imaginary.cash:50004",
+  "wss://blackie.c3-soft.com:50004",
+];
 
 export const testnetServers = ["wss://tbch.loping.net:60004"];
 
