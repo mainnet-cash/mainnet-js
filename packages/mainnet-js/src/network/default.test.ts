457005444b9a64877bb28ad5f8eb4f9f28bf42f6
2qx <2qx@users.noreply.github.com>
2022-05-27 20:47:35 +0000

    Apply formatting changes

--- packages/mainnet-js/src/network/default.test.ts
+++ packages/mainnet-js/src/network/default.test.ts
@@ -27,7 +27,6 @@ test("Should connect to the default testnet cluster", async () => {
   expect(bal).toBeGreaterThan(100);
 });
 
-
 // TODO: Fix timeout issue
 // test("Should connect to the default regtest client", async () => {
 //   let provider = getNetworkProvider("regtest");
