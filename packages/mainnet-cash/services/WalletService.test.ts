235a1bfb88ed43997301ee4b9194ba6b44735d84
2qx <2qx@small.neomailbox.ch>
2022-09-20 10:43:20 -0400

    fix issue with init blcok hash

--- packages/mainnet-cash/services/WalletService.test.ts
+++ packages/mainnet-cash/services/WalletService.test.ts
@@ -172,12 +172,53 @@ describe("Test Wallet Endpoints", () => {
    * getHistory
    */
    it("Should return a wallet history", async () => {
+    const bobsWalletResp = await request(app).post("/wallet/create").send({
+      name: "bobs wallet",
+      network: "regtest",
+    });
+
+    const bobsCashaddr = bobsWalletResp.body.cashaddr;
+
+    await request(app)
+      .post("/wallet/send")
+      .send({
+        walletId: `wif:regtest:${process.env.PRIVATE_WIF}`,
+        to: [
+          {
+            cashaddr: bobsCashaddr,
+            unit: 'satoshis',
+            value: 6000,
+          },
+          {
+            cashaddr: bobsCashaddr,
+            unit: 'satoshis',
+            value: 6000,
+          },
+          {
+            cashaddr: bobsCashaddr,
+            unit: 'satoshis',
+            value: 6000,
+          },
+          {
+            cashaddr: bobsCashaddr,
+            unit: 'satoshis',
+            value: 6000,
+          },
+          {
+            cashaddr: bobsCashaddr,
+            unit: 'satoshis',
+            value: 6000,
+          },
+        ],
+      });
+
+
     let resp = await request(app).post("/wallet/get_history").send({
-      walletId:
-        `wif:regtest:${process.env.PRIVATE_WIF}`,
+      walletId: bobsWalletResp.body.walletId,
     });
     const body = resp.body;
 
+    console.log(JSON.stringify(resp,undefined,2))
     expect(resp.statusCode).toBe(200);
     expect(
       body!.transactions!.length
