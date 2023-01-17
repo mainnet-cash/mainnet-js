7958d586a3dc5654d58749549bbaef9fa67490d9
mainnet-pat <mainnet_pat@protonmail.com>
2023-01-17 14:42:17 +0000

    Minimal necessary changes for vue project with vite and rollup to support mainnet-js

--- demo/vue-vite/src/App.vue
+++ demo/vue-vite/src/App.vue
@@ -1,23 +1,39 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import { Wallet } from "mainnet-js";
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "wallet",
+
+  data(_props){
+    return {
+      wallet: null as (Wallet | null),
+      image: null as any
+    }
+  },
+
+  async mounted() {
+    this.wallet = await Wallet.newRandom()
+    this.image = this.wallet.getDepositQr();
+  },
+});
+
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <img v-if="image" :src="image.src" :alt="image.alt" :title="image.title">
+      <div v-if="wallet">{{ wallet.cashaddr }}</div>
     </div>
   </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
