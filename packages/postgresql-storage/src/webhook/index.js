63fb7f24c4cef4cd1e5c4e76feee5285a262f320
mainnet-pat <mainnet_pat@protonmail.com>
2023-09-12 14:29:19 +0000

    Remove compilation artifacts

--- packages/postgresql-storage/src/webhook/index.js
+++
@@ -1,5 +0,0 @@
-export { default as WebhookWorker } from "./WebhookWorker.js";
-export * from "./Webhook.js";
-export * from "./WebhookBch.js";
-export * from "./interface.js";
-//# sourceMappingURL=index.js.map
