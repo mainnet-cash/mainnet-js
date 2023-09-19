b20c0157696e6cfb62f2caab1a2166eb3927a41c
mainnet-pat <mainnet_pat@protonmail.com>
2023-09-19 13:55:12 +0000

    Put mocks into bundle

--- packages/mainnet-js/src/index.ts
+++ packages/mainnet-js/src/index.ts
@@ -1,5 +1,6 @@
 export * from "./db/index.js";
 export * from "./mine/index.js";
+export * from "./test/fetch.js";
 export * from "./wallet/Bcmr.js";
 
 export * from "./network/index.js";
--- packages/mainnet-js/src/index.webtests.ts
+++ packages/mainnet-js/src/index.webtests.ts
@@ -1,5 +1,4 @@
 export * from "./index.js";
 
 // test utils
-export * from "./test/fetch.js";
 export * from "./test/expect.js";
