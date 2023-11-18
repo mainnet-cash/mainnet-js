b21bc0207772eb440d90e98a19f61353a82ecee2
sploit <acidsploit@users.noreply.github.com>
2023-11-18 21:26:01 +0100

    config DefaultIpfsGateway option

--- packages/mainnet-js/src/config.ts
+++ packages/mainnet-js/src/config.ts
@@ -2,7 +2,7 @@ export class Config {
   // enforces all token-related methods to specify tokenaddr as recepient and change cashaddr
   static EnforceCashTokenReceiptAddresses = false;
   static DefaultParentDerivationPath = "m/44'/0'/0'";
-  static DefaultIpfsGateway = "https://ipfs.io/ipfs/";
+  static DefaultIpfsGateway = "https://dweb.link/ipfs/";
 
   public static setIpfsGateway(ipfsGateway: string) {
     this.DefaultIpfsGateway = ipfsGateway;
