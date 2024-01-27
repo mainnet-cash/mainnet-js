f5575b3f638dd57ed2ab1f1bfdb613e9756227ce
2qx <2qx@users.noreply.github.com>
2024-01-27 21:15:13 +0000

    Apply formatting changes

--- packages/mainnet-js/src/config.ts
+++ packages/mainnet-js/src/config.ts
@@ -1,6 +1,6 @@
 import { wordlist as english } from "@scure/bip39/wordlists/english";
 import { WORDLIST_CHECKSUMS } from "./constant.js";
-import { sha256, binToHex, utf8ToBin} from "@bitauth/libauth";
+import { sha256, binToHex, utf8ToBin } from "@bitauth/libauth";
 
 export class Config {
   // enforces all token-related methods to specify tokenaddr as recepient and change cashaddr
@@ -13,13 +13,14 @@ export class Config {
     this.DefaultIpfsGateway = ipfsGateway;
   }
 
-  public static setWordlist(wordlist: string[]){
-    let checksum = binToHex(sha256.hash(utf8ToBin(wordlist.join(" ")))) 
-    if(!Object.values(WORDLIST_CHECKSUMS).includes(checksum)) throw Error("Setting a custom word list is not allowed")
-    Config.DefaultWordlist = wordlist
+  public static setWordlist(wordlist: string[]) {
+    let checksum = binToHex(sha256.hash(utf8ToBin(wordlist.join(" "))));
+    if (!Object.values(WORDLIST_CHECKSUMS).includes(checksum))
+      throw Error("Setting a custom word list is not allowed");
+    Config.DefaultWordlist = wordlist;
   }
 
-  public static getWordlist(): string[]{
-    return [... Config.DefaultWordlist]
+  public static getWordlist(): string[] {
+    return [...Config.DefaultWordlist];
   }
 }
