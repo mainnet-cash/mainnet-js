08407f94c900259c77a770a21f05d7fb3d36ec56
2qx <2qx@small.neomailbox.ch>
2024-01-27 16:18:20 -0500

    Make error clearer

--- packages/mainnet-js/src/config.test.ts
+++ packages/mainnet-js/src/config.test.ts
@@ -24,14 +24,16 @@ test("Should get the default wordlist", () => {
 });
 
 test("Expect Error setting a bad wordlist to the config", async () => {
-  expect.assertions(1);
-  try {
-    let badList = [...english];
-    badList.pop();
-    expect(Config.setWordlist(badList));
-  } catch (e: any) {
-    expect(e.message).toBe("Setting a custom word list is not allowed");
-  }
+    expect.assertions(1);
+    try {
+        let badList = [...english]
+        badList.pop()
+        expect(Config.setWordlist(badList))
+    } catch (e: any) {
+        expect(e.message).toBe(
+            "Error matching provided wordlist to a known list, see @scure/bip39/wordlists"
+        );
+    }
 });
 
 test("Should get the default wordlist", () => {
