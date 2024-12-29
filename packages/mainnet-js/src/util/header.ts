95596580d453c85f71c212d630513a41d91f298b
mainnet-pat <mainnet-pat@users.noreply.github.com>
2024-12-29 20:19:19 +0000

    Apply formatting changes

--- packages/mainnet-js/src/util/header.ts
+++ packages/mainnet-js/src/util/header.ts
@@ -1,16 +1,25 @@
-import { assertSuccess, binToHex, hexToBin, readUint32LE } from "@bitauth/libauth";
+import {
+  assertSuccess,
+  binToHex,
+  hexToBin,
+  readUint32LE,
+} from "@bitauth/libauth";
 import { HeaderI, HexHeaderI } from "../interface";
 
 export const decodeHeader = (hexHeader: HexHeaderI): HeaderI => {
   const result = {} as HeaderI;
 
   const header = hexToBin(hexHeader.hex);
-  result.version = assertSuccess(readUint32LE({bin: header, index: 0})).result;
+  result.version = assertSuccess(
+    readUint32LE({ bin: header, index: 0 })
+  ).result;
   result.previousBlockHash = binToHex(header.slice(4, 36).reverse());
   result.merkleRoot = binToHex(header.slice(36, 68).reverse());
-  result.timestamp = assertSuccess(readUint32LE({bin: header, index: 68})).result;
-  result.bits = assertSuccess(readUint32LE({bin: header, index: 72})).result;
-  result.nonce = assertSuccess(readUint32LE({bin: header, index: 76})).result;
+  result.timestamp = assertSuccess(
+    readUint32LE({ bin: header, index: 68 })
+  ).result;
+  result.bits = assertSuccess(readUint32LE({ bin: header, index: 72 })).result;
+  result.nonce = assertSuccess(readUint32LE({ bin: header, index: 76 })).result;
   result.height = hexHeader.height;
 
   return result;
