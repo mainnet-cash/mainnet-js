9d6a579804f6c383c67eac23736f0fab64c226b0
mainnet-pat <mainnet_pat@protonmail.com>
2025-07-15 19:14:40 +0000

    Switch to libauth's bip39 implementation, remove wordlist support

--- packages/mainnet-js/src/constant.ts
+++ packages/mainnet-js/src/constant.ts
@@ -25,19 +25,3 @@ export const DERIVATION_PATHS = [
   "m/44'/245'/0'",
   "m/44'/245'/0'/0",
 ];
-
-export const WORDLIST_CHECKSUMS = {
-  czech: "92e035ca0e3680fab85fa18b9e5c7e7582b3bd9caeb6d4bc8a2ecc8e492100c9",
-  english: "f18b9a84c83e38e98eceb0102b275e26438af83ab08f080cdb780a2caa9f3a6d",
-  french: "fd4cd57c8e86a99ac53cd0f21a6b89af366769b0143ac0d268ac7d7a39200145",
-  italian: "d9664953fe4c49e0b40eb6f6378c421f5f01dc9360aaac6d2a3c294f046ef520",
-  japanese: "e641781d89213031233ebc5130020c3de297cad96065ecc688d1c00e010ea71c",
-  korean: "0c26059ed7ede977d7fa1c40443e71793e7850aa6a3d8aabf0cbcec91c1f95ec",
-  portuguese:
-    "85fea658f90c42c182823807a1d30db587abb72c14a3098e1a65f8b78d1ffcf4",
-  simplifiedChinese:
-    "a86e2bd870e228d19a887c9faae8c1374b4e624e85d2b245b59e36583cba4afc",
-  spanish: "27e99ad4328299108663c19eb611310bd3b77260af852169108713019831d07d",
-  traditionalChinese:
-    "11ef479f2f44b6d4f7fb6239dff06e3cd7a1473b83df6cf91adcbbbee598acf6",
-};
