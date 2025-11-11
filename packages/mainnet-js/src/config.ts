4254a5f6f4b7c7d5feaadf40dcdb43d18ae17cad
mainnet-pat <mainnet_pat@protonmail.com>
2025-11-11 14:44:55 +0000

    Switch to libauth's bip39 implementation, remove wordlist support

--- packages/mainnet-js/src/config.ts
+++ packages/mainnet-js/src/config.ts
@@ -15,4 +15,9 @@ export class Config {
   public static setIpfsGateway(ipfsGateway: string) {
     this.DefaultIpfsGateway = ipfsGateway;
   }
+
+  // custom exchange rate function
+  public static GetExchangeRateFn:
+    | ((symbol: string) => Promise<number>)
+    | undefined = undefined;
 }
